c1404357ec923b70dc7cb54e11a5ab9b7922ddf3
Moaz Baghdadi <moazbaghdadi@gmail.com>
2025-09-12 18:28:30 +0200

    refactor assignments

--- src/features/assignment-responses/dto/start-assignment.response.dto.ts
+++ src/features/assignment-responses/dto/start-assignment.response.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { IsDate } from 'class-validator'
-import { AssignmentFormDto } from '../../assignments/dto/assignment-form.dto'
+import { AssignmentFormDto } from './assignment-form.dto'
 
 export class StartAssignmentResponseDto extends AssignmentFormDto {
     @ApiProperty({ type: Date })
